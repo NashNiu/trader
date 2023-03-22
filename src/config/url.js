7fe39526e316c9311830ee1f5afd68fb6a8e816b
niutengfei <niutengfei@magiccompass.cn>
2023-03-22 17:28:31 +0800

    链接替换

--- src/config/url.js
+++ src/config/url.js
@@ -2,5 +2,5 @@ export default {
   chartUrl: 'https://www.cd-ex.com/tradecdex/#/',
   socketUrl: `${location.host}/wss/api/lp/auth/websocks/mt5sock`,
   klineHistoryDataUrl: '/kline/TradeInfo/ChartRequestBinary',
-  helpUrl: 'https://www.magiccompasssec.com/TC/app',
+  helpUrl: 'https://support.mcgrp.com', // 帮助中心
 };

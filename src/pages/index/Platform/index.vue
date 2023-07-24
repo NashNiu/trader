5bf5d393c9314df5bbf3e6fb4203ef3d7db22773
niutengfei <niutengfei@magiccompass.cn>
2023-07-24 11:11:49 +0800

    platform 翻译

--- src/pages/index/Platform/index.vue
+++ src/pages/index/Platform/index.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="platform">
     <div class="banner">
-      <h4>MT5交易平台</h4>
+      <h4>{{ t('plat.mt5') }}</h4>
       <p>
-        FxPro MT5交易平台秉承MT4的优势，为您提供交易金融市场所需的一切。
-        MT5易于使M
+        {{ t('plat.title1') }}
         <br />
-        用，且完全根据您的交易风格定制方案, 具有EA开发的先进环境和其他功能。
+        {{ t('plat.title2') }}
       </p>
     </div>
     <div class="width_1270 once-view">
@@ -16,15 +15,14 @@
         </el-col>
         <el-col :md="12">
           <div class="once-view_button">
-            <p>立即交易</p>
+            <p>{{ t('plat.tradeNow') }}</p>
             <p>
-              或者
-              <span>尝试模拟账号</span>
+              {{ t('plat.or') }}
+              <span>{{ t('plat.title2') }}</span>
             </p>
           </div>
           <p style="font-size: 16px; color: #2963a4">
-            外汇，贵金属，指数，能源，股票&期货• 竞争性点差与0佣金•
-            市场执行•净值结算能力
+            {{ t('plat.p1') }}
           </p>
         </el-col>
       </el-row>
@@ -32,12 +30,12 @@
     <div class="width_1270 MT5Model">
       <div id="MT5" class="MT5Model_1">
         <div class="MT5Model_1_title">
-          <h1>MetaTrader 5 - 下载</h1>
-          <h2>适用于电脑,Mac,安卓，手提电脑，网页，Windows版</h2>
+          <h1>{{ t('plat.mt5Down') }}</h1>
+          <h2>{{ t('plat.applyTo') }}</h2>
           <p>
-            MT5已成为业内使用最广泛的平台之一，它融合了MT4多年来一直拥有的所有关键功
+            {{ t('plat.wide') }}，
             <br />
-            能，还具有一些额外的功能和针对EA交易的优化环境。
+            {{ t('plat.extra') }}。
           </p>
         </div>
       </div>
@@ -46,31 +44,31 @@
           <div class="list-model">
             <div>
               <img src="../../../assets/img/newIndex/dui.png" />
-              <span>38+预先安装技术指标</span>
+              <span>{{ t('plat.indicator') }}</span>
             </div>
             <div>
               <img src="../../../assets/img/newIndex/dui.png" />
-              <span>44种分析图表工具</span>
+              <span>{{ t('plat.tools') }}</span>
             </div>
             <div>
               <img src="../../../assets/img/newIndex/dui.png" />
-              <span>3种图表类型&21种时间框架</span>
+              <span>{{ t('plat.frame') }}</span>
             </div>
             <div>
               <img src="../../../assets/img/newIndex/dui.png" />
-              <span>其他挂单类型（卖出止损限价&买入止损限价）</span>
+              <span>{{ t('plat.orderType') }}</span>
             </div>
             <div>
               <img src="../../../assets/img/newIndex/dui.png" />
-              <span>可拆分图表</span>
+              <span>{{ t('plat.chart') }}</span>
             </div>
             <div>
               <img src="../../../assets/img/newIndex/dui.png" />
-              <span>从图表中选择一键交易&普通交易</span>
+              <span>{{ t('plat.trade') }}</span>
             </div>
             <div>
               <img src="../../../assets/img/newIndex/dui.png" />
-              <span>追踪止损</span>
+              <span>{{ t('plat.trackLoss') }}</span>
             </div>
           </div>
         </el-col>
@@ -78,27 +76,27 @@
           <div class="list-model">
             <div>
               <img src="../../../assets/img/newIndex/dui.png" />
-              <span>易于使用的界面</span>
+              <span>{{ t('plat.interface') }}</span>
             </div>
             <div>
               <img src="../../../assets/img/newIndex/dui.png" />
-              <span>完全可定制的图表</span>
+              <span>{{ t('plat.customizable') }}</span>
             </div>
             <div>
               <img src="../../../assets/img/newIndex/dui.png" />
-              <span>添加定制EA&指标</span>
+              <span>{{ t('plat.EA') }}</span>
             </div>
             <div>
               <img src="../../../assets/img/newIndex/dui.png" />
-              <span>综合经济日历</span>
+              <span>{{ t('plat.calendar') }}</span>
             </div>
             <div>
               <img src="../../../assets/img/newIndex/dui.png" />
-              <span>DOM(市场深度)</span>
+              <span>{{ t('plat.DOM') }}</span>
             </div>
             <div>
               <img src="../../../assets/img/newIndex/dui.png" />
-              <span>适用于Windows或Mac</span>
+              <span>{{ t('plat.OS') }}</span>
             </div>
           </div>
         </el-col>
@@ -110,25 +108,25 @@
         <el-col :md="9" :push="3" class="MT5Model_down_right">
           <div>
             <h4>
-              免费下载以充分利用它与FxPro MT5交易
-              <br />
-              帐户一起提供的所有优势。
+              {{ t('plat.freeDown') }}
             </h4>
-            <div class="down_button">下载MT5 适用于Windows</div>
-            <div class="down_button down_button_black">下载MT5 适用于MAC</div>
+            <div class="down_button">{{ t('plat.forWin') }}</div>
+            <div class="down_button down_button_black">
+              {{ t('plat.forMac') }}
+            </div>
             <div class="down_table">
               <div>
-                <p>系统要求:</p>
+                <p>{{ t('plat.osRequire') }}:</p>
                 <p>
-                  兼容微软Windows 2008/7/8/10 操作系统，网速在56 kbps或以上。
+                  {{ t('plat.requirement') }}
                 </p>
               </div>
               <div>
-                <p>请注意:</p>
+                <p>{{ t('plat.attention') }}:</p>
                 <p>
-                  FxPro MT5帐户默认设置为“净额结算”，这意味着交易是被汇总的，
+                  {{ t('plat.a1') }}，
                   <br />
-                  且无法进行对冲，尽管我们可以根据要求提供平台的“对冲”版本。
+                  {{ t('plat.a2') }}。
                 </p>
               </div>
             </div>
@@ -137,11 +135,9 @@
       </el-row>
       <div class="MT5Model_tool">
         <div class="MT5Model_title">
-          <h1>MT5 交易中心工具</h1>
+          <h1>{{ t('plat.tradeCenter') }}</h1>
           <p>
-            使用交易中心提供的独家技术分析指标来提升您的交易水平。识别新的交易机会，改善您的出入场时机，并即时了解交易中心的首选方
-            <br />
-            向以及目标水平和基于支持线和价格轴线的替代方案。
+            {{ t('plat.desc1') }}
           </p>
         </div>
         <div class="MT5Model_tool_b">
@@ -151,27 +147,26 @@
       </div>
       <div class="MT5Model_trading">
         <div class="MT5Model_title">
-          <h1>使用 MT5 进行自动交易</h1>
-          <p>Meta编辑器 • 策略测试器 • MclpPro</p>
+          <h1>{{ t('plat.autoTrade') }}</h1>
+          <p>{{ t('plat.meta') }}</p>
         </div>
         <div class="MT5Model_trading_button">
           <div>
             <img src="../../../assets/img/newIndex/document.png" />
-            <span>下载回测用户指南</span>
+            <span>{{ t('plat.userGuide') }}</span>
           </div>
           <div>
             <img src="../../../assets/img/newIndex/document.png" />
-            <span>下载EA优化用户指南</span>
+            <span>{{ t('plat.eaGuide') }}</span>
           </div>
         </div>
         <!-- <div class="MT5Model_trading_switch"></div> -->
       </div>
       <div id="Mobile" class="MT5Model_mobile">
         <div class="MT5Model_title">
-          <h1>MT5平台手机&平板端</h1>
+          <h1>{{ t('plat.mt5Mobile') }}</h1>
           <p>
-            使用MclpPro
-            MT5移动版，您可以随时随地在手机上使用最受欢迎的交易平台。适用于苹果与安卓系统。
+            {{ t('plat.mclpPro') }}
           </p>
         </div>
         <el-row class="MT5Model_mobile_model" align="middle">
@@ -188,19 +183,19 @@
             <div class="list-model list-model_height">
               <div>
                 <img src="../../../assets/img/newIndex/dui.png" />
-                <span>拥有9种时间框架的3种图表类型</span>
+                <span>{{ t('plat.timeFrame') }}</span>
               </div>
               <div>
                 <img src="../../../assets/img/newIndex/dui.png" />
-                <span>30种技术指标& 23种分析工具</span>
+                <span>{{ t('plat.analysisTool') }}</span>
               </div>
               <div>
                 <img src="../../../assets/img/newIndex/dui.png" />
-                <span>自定义图表显示&图表交易</span>
+                <span>{{ t('plat.chartTrade') }}</span>
               </div>
               <div>
                 <img src="../../../assets/img/newIndex/dui.png" />
-                <span>市场新闻摘要</span>
+                <span>{{ t('plat.headline') }}</span>
               </div>
             </div>
           </el-col>
@@ -208,19 +203,19 @@
             <div class="list-model list-model_height">
               <div>
                 <img src="../../../assets/img/newIndex/dui.png" />
-                <span>易于使用的界面</span>
+                <span>{{ t('plat.interface') }}</span>
               </div>
               <div>
                 <img src="../../../assets/img/newIndex/dui.png" />
-                <span>MclpPro电邮</span>
+                <span>{{ t('plat.email') }}</span>
               </div>
               <div>
                 <img src="../../../assets/img/newIndex/dui.png" />
-                <span>自定义推送通知</span>
+                <span>{{ t('plat.notice') }}</span>
               </div>
               <div>
                 <img src="../../../assets/img/newIndex/dui.png" />
-                <span>平板电脑优化显示界面</span>
+                <span>{{ t('plat.optimizedInterface') }}</span>
               </div>
             </div>
           </el-col>
@@ -228,14 +223,19 @@
       </div>
       <div id="WAB" class="MT5Model_wab">
         <div class="MT5Model_title">
-          <h1>MT5网页版</h1>
-          <p>无需安装任何软件即可直接通过浏览器登录MT5账号。。</p>
+          <h1>
+            <span>{{ t('plat.web') }}</span>
+          </h1>
+          <p>
+            <span>{{ t('plat.withoutInstalling') }}</span>
+            。。
+          </p>
         </div>
         <div class="MT5Model_wab_button">
           <img src="../../../assets/img/newIndex/P_wab_button.png" />
           <div class="MT5Model_wab_button_text">
-            <h4>系统要求:</h4>
-            <p>通过您任意设备的网页浏览器均可登录 (建议您的浏览器为最新版本)</p>
+            <h4>{{ t('plat.osRequire') }}:</h4>
+            <p>{{ t('plat.requirement2') }}</p>
           </div>
         </div>
         <el-row :gutter="40">
@@ -243,15 +243,15 @@
             <div class="list-model list-model_height">
               <div>
                 <img src="../../../assets/img/newIndex/dui.png" />
-                <span>30个技术指标与24种分析工具</span>
+                <span>{{ t('plat.analysisTool') }}</span>
               </div>
               <div>
                 <img src="../../../assets/img/newIndex/dui.png" />
-                <span>拥有9种时间框架的3种图表类型</span>
+                <span>{{ t('plat.timeFrame') }}</span>
               </div>
               <div>
                 <img src="../../../assets/img/newIndex/dui.png" />
-                <span>单击交易</span>
+                <span>{{ t('plat.clickTrade') }}</span>
               </div>
             </div>
           </el-col>
@@ -259,15 +259,15 @@
             <div class="list-model list-model_height">
               <div>
                 <img src="../../../assets/img/newIndex/dui.png" />
-                <span>易于使用的界面</span>
+                <span>{{ t('plat.interface') }}</span>
               </div>
               <div>
                 <img src="../../../assets/img/newIndex/dui.png" />
-                <span>自定义图表显示</span>
+                <span>{{ t('plat.customizeDisplay') }}</span>
               </div>
               <div>
                 <img src="../../../assets/img/newIndex/dui.png" />
-                <span>安全数据传输</span>
+                <span>{{ t('plat.safeData') }}</span>
               </div>
             </div>
           </el-col>
@@ -275,10 +275,9 @@
         <div class="MT5Model_wab_button">
           <img src="../../../assets/img/newIndex/P_wab_button.png" />
           <div class="MT5Model_wab_button_text">
-            <h2>不确定MT5是否适合您？</h2>
+            <h2>{{ t('plat.notSure') }}？</h2>
             <div>
-              比较MclpPro MT4, MT5
-              以及cTrader交易账号&平台的特点，查看对比表格。
+              {{ t('plat.compare') }}
             </div>
           </div>
           <img
@@ -286,52 +285,42 @@
             class="button"
           />
           <div class="phone">
-            仍对MT5有疑问? 您可以通过
-            <span>在线咨询</span>
+            {{ t('plat.doubt') }}
+            <span>{{ t('plat.onlineConsultant') }}</span>
             ，
-            <span>电邮</span>
-            或
-            <span>免费电话</span>
-            联系24/5客服团队。
+            <span>{{ t('plat.e_mail') }}</span>
+            {{ t('plat.or') }}
+            <span>{{ t('plat.freePhone') }}</span>
+            {{ t('plat.supportTeam') }}
           </div>
         </div>
         <el-row :gutter="40" class="MT5Model_wab_introduce">
           <el-col :md="6">
             <img src="../../../assets/img/newIndex/wab_bar.png" />
-            <h4>10+历史老牌经纪商</h4>
+            <h4>10+{{ t('plat.broker1') }}</h4>
             <p>
-              一个真正全球化经纪商
-              <br />
-              MCLP多年来迅速成长，并以为客户
-              <br />
-              提供顶级交易条件。
+              {{ t('plat.broker2') }}
             </p>
           </el-col>
           <el-col :md="6">
             <img src="../../../assets/img/newIndex/wab_bar_2.png" />
-            <h4>无缝交易体验</h4>
+            <h4>{{ t('plat.experience') }}</h4>
             <p>
-              100+英国&国际奖项
-              <br />
-              MCLP一直以来得到行业的认可，迄
-              <br />
-              今为止，由于其平台与服务，已获得
-              <br />
-              67 个奖项。
+              {{ t('plat.awards') }}
             </p>
           </el-col>
           <el-col :md="6">
             <img src="../../../assets/img/newIndex/wab_bar_3.png" />
-            <h4>致力于透明化</h4>
+            <h4>{{ t('plat.transparency') }}</h4>
             <p>
-              值得信赖并受全监管
+              {{ t('plat.supervise') }}
               <br />
-              我们坚持公平与卓越的交易体验。
+              {{ t('plat.justice') }}
             </p>
           </el-col>
           <el-col :md="6">
             <img src="../../../assets/img/newIndex/wab_bar_4.png" />
-            <h4>一流的MT5经纪商</h4>
+            <h4>{{ t('plat.mt5Broker') }}</h4>
             <p>
               2020
               <br />
@@ -340,15 +329,11 @@
           </el-col>
         </el-row>
         <div class="MT5Model_wab_bottom">
-          <h4>
-            免费下载MT5平台并在MCLP开启
-            <br />
-            交易之旅。
-          </h4>
-          <div class="button">立即交易</div>
+          <h4>{{ t('plat.tradingTour') }}</h4>
+          <div class="button">{{ t('plat.tradeNow') }}</div>
           <div class="text">
-            或者
-            <span>尝试模拟账号</span>
+            {{ t('plat.or') }}
+            <span>{{ t('plat.tryDemo') }}</span>
           </div>
         </div>
       </div>
@@ -358,6 +343,8 @@
 </template>
 <script setup>
 import Model_4 from '../model_4.vue';
+import { useI18n } from 'vue-i18n';
+const { t } = useI18n();
 </script>
 <style lang="less" scoped>
 .banner {
